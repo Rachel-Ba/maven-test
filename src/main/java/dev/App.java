f1be9c1dc301461989747dd6259884b4e5494ecf
Rachel BANDIERA <rachel.bandiera@gmail.com>
2020-06-30 20:35:57 +0200

    Maven Plugin

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,9 +1,18 @@
 package dev;
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 public class App {
   public static void main(String[] args) {
-    String asciiArt = FigletFont.convertOneLine("PATATE");
+	ResourceBundle bundle = ResourceBundle.getBundle("application");
+	String titre = bundle.getString("titre");
+	
+    String asciiArt = FigletFont.convertOneLine(titre);
     System.out.println(asciiArt);
+    
+    String environnement = ResourceBundle.getBundle("application").getString(
+    		"environnement");
+    System.out.println("Environnement : " + environnement);
   }
 }
